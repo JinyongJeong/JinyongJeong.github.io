aed0827e2e4e2cdc2d09889a8cb17bb20cf7913d
JinyongJeong <jjy0923@gmail.com>
2020-06-23 17:10:32 +0900

    Update sh

--- notion_to_github_pages.sh
+++ notion_to_github_pages.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# Author: Kim Hangil(uoneway). 
-# URL: https://github.com/uoneway/Notion-to-GitHub-Pages
-# Contact: uoneway@gmail.com
-
 
 # REPLACE THIS as your github.io structure
-posts_folder_path='_posts' # 여기서 /을 앞에 넣으면 안됨. 이미지 경로 할때는 앞에 / 넣어줘야 url상에서 image폴더 찾을 수 있지만 로컬에서 실행할때는 루트로 들어가기에...
+posts_folder_path='_posts' 
 images_folder_path='images/post'
 
 # Name regexp of exported zip file from Notion
